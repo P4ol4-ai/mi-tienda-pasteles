e26ca59502561fec01f1c66593d3e9b7468f43ac
Paola Orellana <paola.orellanaajhuacho@intjem.edu.bo>
2026-08-03 08:44:56 -0600

    Configurar base para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/mi-tienda-pasteles/', // 👈 AGREGA ESTA LÍNEA
   plugins: [
     vue(),
     vueDevTools(),
